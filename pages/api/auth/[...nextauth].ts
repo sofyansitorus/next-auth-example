d0ad40c681743ef9226337c9c2149f39898d5871
Sofyan Sitorus <sofyansitorus@gmail.com>
2022-02-06 09:08:21 +0700

    Update [...nextauth].ts

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -41,14 +41,14 @@ export default NextAuth({
         username: { label: 'Username', type: 'text', placeholder: 'jsmith' },
         password: { label: 'Password', type: 'password' },
       },
-      async authorize({ username }, req) {
+      async authorize() {
         // You need to provide your own logic here that takes the credentials
         // submitted and returns either a object representing a user or value
         // that is false/null if the credentials are invalid.
         // e.g. return { id: 1, name: 'J Smith', email: 'jsmith@example.com' }
         // You can also use the `req` object to obtain additional parameters
         // (i.e., the request IP address)
-        const res = await fetch(`https://api.github.com/users/${username}`, {
+        const res = await fetch('https://api.github.com/users/sofyansitorus', {
           method: 'GET',
         });
 
